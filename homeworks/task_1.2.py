0b3c5ed77306d826122cd6f169bb36fbdca1bb23
Step0813 <svgrischenko@mail.ru>
2023-05-03 10:08:03 +0700

    решено

--- homeworks/task_1.2.py
+++ homeworks/task_1.2.py
@@ -1,5 +1,8 @@
 # Задача 1.2.
 
+from random import sample
+from datetime import timedelta
+
 # Пункт A. 
 # Приведем плейлист песен в виде списка списков
 # Список my_favorite_songs содержит список названий и длительности каждого трека
@@ -18,6 +21,15 @@ my_favorite_songs = [
     ['In This World', 4.02],
 ]
 
+shuffle_songs = sample(my_favorite_songs, 3)
+length = timedelta()
+for song, l in shuffle_songs:
+    m, s = '{:.2f}'.format(l).split('.')
+    ms = timedelta(minutes=int(m), seconds=int(s))
+    length += ms
+print(f'Три песни звучат {str(length)[2:]}')
+
+
 # Пункт B. 
 # Есть словарь песен 
 # Распечатайте общее время звучания трех случайных песен
@@ -34,6 +46,14 @@ my_favorite_songs_dict = {
     'Nowhere to Run': 2.58,
     'In This World': 4.02,
 }
+
+songs_list = sample(list(my_favorite_songs_dict.items()), 3)
+length2 = timedelta()
+for song, l in songs_list:
+    m, s = '{:.2f}'.format(l).split('.')
+    ms = timedelta(minutes=int(m), seconds=int(s))
+    length2 += ms
+print(f'Три песни звучат {str(length2)[2:]}')
 
 # Дополнительно для пунктов A и B
 # Пункт C.
